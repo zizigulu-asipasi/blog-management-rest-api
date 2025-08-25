d2f720fcf7507ec18b07099457ffafe406f4d935
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-08-25 07:15:00 +0330

    feat: inhance Param and Query decorators in the users controller (getUsers and createUsers)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,15 +3,15 @@ import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
   @Get('/:id{/:optional}')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() body: any) {
-    console.log(body);
+  public createUsers(@Body() request: any) {
+    console.log(request);
     return 'You sent a post request to users endpoint';
   }
 }
